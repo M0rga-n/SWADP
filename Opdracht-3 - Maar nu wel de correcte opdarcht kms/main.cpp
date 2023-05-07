16e57f6ce91a8dd5bb655110ccd356cd7447cac5
M0rga-n <verburgnoah@gmail.com>
2023-05-07 21:58:08 +0200

    Finished opdracht 3

---
+++ Opdracht-3 - Maar nu wel de correcte opdarcht kms/main.cpp
@@ -0,0 +1,142 @@
+// ================================================================
+// Infrastructure Layer
+// ================================================================
+
+#include <iostream>
+#include <list>
+#include <string>
+
+class Subject;
+
+class Observer
+{
+private:
+    Subject* m_Subject;
+
+protected:
+    Subject* getSubject() const { return m_Subject; }
+
+public:
+    Observer(Subject*);
+    virtual ~Observer();
+    virtual void update () =0;
+};
+
+class Subject
+{
+private:
+    std::list<Observer*> m_List;
+
+protected:
+    virtual void notify();
+
+public:
+    virtual ~Subject() {};
+    virtual void insert(Observer* observer) { m_List.push_back(observer); }
+    virtual void remove(Observer* observer) { m_List.remove(observer); }
+};
+
+Observer::Observer(Subject* subject) : m_Subject(subject)
+{
+    getSubject()->insert(this);
+}
+
+Observer::~Observer()
+{
+    getSubject()->remove(this);
+}
+
+void Subject::notify()
+{
+    for (auto observer : m_List)
+    {
+        observer->update();
+    };
+}
+
+// ================================================================
+// DomainLayer
+// ================================================================
+
+class Element
+{
+public:
+    virtual ~Element() {}
+    virtual void draw() =0;
+    virtual std::string name() =0;
+};
+
+class Ninja : public Element, public Subject
+{
+private:
+    std::string m_Name;
+    int m_AantalLevens;
+
+public:
+    Ninja(std::string);
+    virtual ~Ninja() {};
+    virtual void draw();
+    virtual std::string name() { return m_Name; }
+    virtual void incLevens();
+    virtual void decLevens();
+};
+
+Ninja::Ninja(std::string name) : m_Name(name)
+{
+    m_AantalLevens = 1;
+}
+
+void Ninja::draw()
+{
+    std::cout << name() << " " << m_AantalLevens << std::endl;
+}
+
+void Ninja::incLevens()
+{
+    m_AantalLevens++;
+    notify();
+}
+
+void Ninja::decLevens()
+{
+    m_AantalLevens--;
+    notify();
+}
+// ================================================================
+// InterfaceLayer
+// ================================================================
+
+class NinjaWindow : public Observer
+{
+private:
+    Ninja* m_Ninja;
+
+public:
+    NinjaWindow(Ninja*);
+    virtual ~NinjaWindow() {}
+    virtual void draw() { m_Ninja->draw(); }
+    virtual void update() { draw(); }
+};
+
+NinjaWindow::NinjaWindow(Ninja* ninja) : Observer(ninja), m_Ninja(ninja)
+{
+
+}
+
+int main()
+{
+    Ninja h("henk"), j("joop");
+
+    NinjaWindow hW(&h), jW(&j);
+
+    h.incLevens();
+    h.incLevens();
+    j.incLevens();
+    j.incLevens();
+    j.incLevens();
+    h.incLevens();
+    j.decLevens();
+    h.decLevens();
+
+    return 0;
+}
