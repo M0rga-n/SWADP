6a77723669b02da7cf62eaf6fd4c2feceba010a5
M0rga-n <verburgnoah@gmail.com>
2023-05-18 00:18:52 +0200

    cleanup

--- Opdracht-3 - Maar nu wel de correcte opdarcht kms/main.cpp
+++ Opdracht-3 - Maar nu wel de correcte opdarcht kms/main.cpp
@@ -2,12 +2,15 @@
 // Infrastructure Layer
 // ================================================================
 
+//----------------------------------------------------------------------
 #include <iostream>
 #include <list>
 #include <string>
 
+//----------------------------------------------------------------------
 class Subject;
 
+//----------------------------------------------------------------------
 class Observer
 {
 private:
@@ -22,105 +25,138 @@ public:
     virtual void update () =0;
 };
 
+//----------------------------------------------------------------------
 class Subject
 {
-private:
-    std::list<Observer*> m_List;
+
+public:
+    virtual ~Subject() {};
+
+    virtual void insert(Observer* observer) { m_List.push_back(observer); }
+
+    virtual void remove(Observer* observer) { m_List.remove(observer); }
 
 protected:
     virtual void notify();
 
-public:
-    virtual ~Subject() {};
-    virtual void insert(Observer* observer) { m_List.push_back(observer); }
-    virtual void remove(Observer* observer) { m_List.remove(observer); }
+private:
+    std::list<Observer*> m_List;
 };
 
+//----------------------------------------------------------------------
 Observer::Observer(Subject* subject) : m_Subject(subject)
 {
     getSubject()->insert(this);
 }
 
+//----------------------------------------------------------------------
 Observer::~Observer()
 {
     getSubject()->remove(this);
 }
 
+//----------------------------------------------------------------------
 void Subject::notify()
 {
-    for (auto observer : m_List)
+    for (Observer* observer : m_List)
     {
         observer->update();
     };
 }
 
+//----------------------------------------------------------------------
+
 // ================================================================
 // DomainLayer
 // ================================================================
 
+//----------------------------------------------------------------------
 class Element
 {
 public:
     virtual ~Element() {}
+
     virtual void draw() =0;
+
     virtual std::string name() =0;
 };
 
+//----------------------------------------------------------------------
 class Ninja : public Element, public Subject
 {
-private:
-    std::string m_Name;
-    int m_AantalLevens;
-
 public:
     Ninja(std::string);
+
     virtual ~Ninja() {};
+
     virtual void draw() override;
+
     virtual std::string name() override { return m_Name; }
+
     virtual void incLevens();
+
     virtual void decLevens();
+
+private:
+    std::string m_Name;
+
+    int m_AantalLevens;
+
 };
 
+//----------------------------------------------------------------------
 Ninja::Ninja(std::string name) : m_Name(name)
 {
     m_AantalLevens = 1;
 }
 
+//----------------------------------------------------------------------
 void Ninja::draw()
 {
     std::cout << name() << " " << m_AantalLevens << std::endl;
 }
 
+//----------------------------------------------------------------------
 void Ninja::incLevens()
 {
     m_AantalLevens++;
     notify();
 }
 
+//----------------------------------------------------------------------
 void Ninja::decLevens()
 {
     m_AantalLevens--;
     notify();
 }
+//----------------------------------------------------------------------
+
 // ================================================================
 // InterfaceLayer
 // ================================================================
 
+//----------------------------------------------------------------------
+
 class NinjaWindow : public Observer
 {
-
 public:
     NinjaWindow(Ninja*);
+
     virtual ~NinjaWindow() {}
+
     virtual void draw();
+
     virtual void update() override { draw(); }
+
 };
 
+//----------------------------------------------------------------------
 NinjaWindow::NinjaWindow(Ninja* ninja) : Observer(ninja)
 {
 
 }
 
+//----------------------------------------------------------------------
 void NinjaWindow::draw()
 {
     Ninja* ninja = dynamic_cast<Ninja*>(getSubject());
@@ -131,6 +167,7 @@ void NinjaWindow::draw()
     }
 }
 
+//----------------------------------------------------------------------
 int main()
 {
     Ninja h("henk"), j("joop");
